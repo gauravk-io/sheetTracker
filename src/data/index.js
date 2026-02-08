fce44647daeb5a7d59ea1c88ef9b4b1223df9682
gauravk-io <gauravkr7265@gmail.com>
2026-02-08 15:04:11 +0530

    feat: update with latest sheet questions and topics

--- src/data/index.js
+++ src/data/index.js
@@ -12,22 +12,24 @@ import pattern8 from "./patterns/greedy-algorithms.json";
 import pattern9 from "./patterns/hash-maps.json";
 import pattern10 from "./patterns/in-place-reversal-of-a-linkedlist.json";
 import pattern11 from "./patterns/island-matrix-traversal.json";
-import pattern12 from "./patterns/k-way-merge.json";
+// import pattern12 from "./patterns/k-way-merge.json";
 import pattern13 from "./patterns/merge-intervals.json";
-import pattern14 from "./patterns/modified-binary-search.json";
+// import pattern14 from "./patterns/modified-binary-search.json";
 import pattern15 from "./patterns/kadane-pattern.json";
 import pattern16 from "./patterns/prefix-sum.json";
 import pattern17 from "./patterns/sliding-window.json";
 import pattern18 from "./patterns/stack.json";
 import pattern19 from "./patterns/subsets.json";
-import pattern20 from "./patterns/top-k-elements.json";
+// import pattern20 from "./patterns/top-k-elements.json";
 import pattern21 from "./patterns/topological-sort-graph.json";
-import pattern22 from "./patterns/tree-breadth-first-search.json";
-import pattern23 from "./patterns/tree-depth-first-search.json";
+// import pattern22 from "./patterns/tree-breadth-first-search.json";
+// import pattern23 from "./patterns/tree-depth-first-search.json";
 import pattern24 from "./patterns/trie.json";
-import pattern25 from "./patterns/two-heaps.json";
+// import pattern25 from "./patterns/two-heaps.json";
 import pattern26 from "./patterns/two-pointers.json";
 import pattern27 from "./patterns/union-find.json";
+import pattern28 from "./patterns/binary-search.json";
+import pattern29 from "./patterns/heap.json";
 
 // Merge all problems into a single array with auto-generated IDs
 // ID format: pattern-name-index (e.g., "two-pointers-0", "two-pointers-1")
@@ -47,22 +49,24 @@ let globalIndex = 0;
   pattern9,
   pattern10,
   pattern11,
-  pattern12,
+  // pattern12,
   pattern13,
-  pattern14,
+  // pattern14,
   pattern15,
   pattern16,
   pattern17,
   pattern18,
   pattern19,
-  pattern20,
+  // pattern20,
   pattern21,
-  pattern22,
-  pattern23,
+  // pattern22,
+  // pattern23,
   pattern24,
-  pattern25,
+  // pattern25,
   pattern26,
   pattern27,
+  pattern28,
+  pattern29,
 ].forEach((patternProblems) => {
   patternProblems.forEach((problem, index) => {
     // Create a unique ID based on pattern name and index within that pattern
